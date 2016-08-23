156616830e62389a543347763c720bc7f09b45c5
JMPSUser <JMPSUser@WIN-QQD0VUNA56R>
2016-08-24 07:17:44 +1000

    Add project files.

---
+++ run_a_unit_test_from_mtm/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace run_a_unit_test_from_mtm
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
